94ef9474f87910e326df780b2f4ead1ba5be97bf
Nawel <nawel.bouab@gmail.com>
2019-10-03 18:49:04 +0200

    output level4 done

--- level4/level4.py
+++ level4/level4.py
@@ -1,8 +1,13 @@
 import json
 from datetime import datetime
 
-with open('input.json') as json_file:
+
+rentals_list = dict()
+rentals_list["rentals"] = []
+
+with open("input.json") as json_file:
     data = json.load(json_file)
+    #print(data)
     for y in data["rentals"]:
         for x in data["cars"]:
             if x["id"]==y["car_id"]:
@@ -18,14 +23,40 @@ with open('input.json') as json_file:
                     time_component=(number_of_days-1)*0.9*x["price_per_day"]+1*x["price_per_day"]
                 else:
                     time_component=x["price_per_day"]
-
                 distance_component = y["distance"]*x["price_per_km"]
-                rental_price = time_component + distance_component
+                rental_price = int(time_component + distance_component)
+
 
                 commission = rental_price*0.3
-                insurance = commission*0.5
-                road_assistance = number_of_days*100
-                drivy = commission - (insurance+road_assistance)
-                print(insurance)
-                print(road_assistance)
-                print(drivy)
+                insurance = int(commission/2)
+                assistance = number_of_days*100
+                drivy = int(commission - insurance - assistance)
+
+
+                rental = dict()
+                rental["id"] = y["id"]
+                rental["actions"] = []
+
+                for i in ["driver", "owner", "insurance", "assistance", "drivy"]:
+                    action = dict()
+                    action["who"] = i
+                    if i == "driver":
+                        action["type"] = "debit"
+                        action["amount"] = rental_price
+                    else:
+                        action["type"] = "credit"
+                        if i == "owner":
+                            action["amount"] = int(rental_price-commission)
+                        elif i == "insurance":
+                            action["amount"] = insurance
+                        elif i == "assistance":
+                            action["amount"] = assistance
+                        else:
+                            action["amount"] = drivy
+                    rental["actions"].append(action)
+
+
+                rentals_list["rentals"].append(rental)
+
+with open("output.json", "w") as output_file:
+    json.dump(rentals_list, output_file, indent=4)
