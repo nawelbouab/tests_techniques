d91fb90dcf5566d58ec1fc9a80278592feff83ba
Nawel <nawel.bouab@gmail.com>
2019-10-03 14:34:21 +0200

    output level3 done

--- level3/level3.py
+++ level3/level3.py
@@ -1,6 +1,9 @@
 import json
 from datetime import datetime
 
+#Initialisation du dictionnaire principal
+rentals_list = dict()
+rentals_list["rentals"] = []
 with open('input.json') as json_file:
     data = json.load(json_file)
     for y in data["rentals"]:
@@ -22,10 +25,28 @@ with open('input.json') as json_file:
                 distance_component = y["distance"]*x["price_per_km"]
                 rental_price = time_component + distance_component
 
+                #Calcul des commissions
                 commission = rental_price*0.3
-                insurance = commission*0.5
-                road_assistance = number_of_days*100
-                drivy = commission - (insurance+road_assistance)
-                print(insurance)
-                print(road_assistance)
-                print(drivy)
+                insurance = commission/2
+                assistance = number_of_days*100
+                drivy = commission - insurance - assistance
+
+                #Creation du dictionnaire pour les commissions
+                commission_dict = dict()
+                commission_dict["insurance_fee"] = int(insurance)
+                commission_dict["assistance_fee"] = int(assistance)
+                commission_dict["drivy_fee"] = int(drivy)
+
+                #Creation du dictionnaire de la location
+                rental = dict()
+                rental["id"] = y["id"]
+                rental["price"] = rental_price
+                rental["commission"] = commission_dict
+
+                #Ajout dans le dictionnaire principal
+                rentals_list["rentals"].append(rental)
+
+
+#Sauvegarde les resultats dans un JSON
+with open('output.json', 'w') as output_file:
+    json.dump(rentals_list, output_file, indent=4)
